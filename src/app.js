706cf761d30c456c4b4a088143d726b34458948a
Odile-a <odilealvarez@gmail.com>
2023-01-24 00:10:34 +0000

    JS form function for the current temperature and description

--- src/app.js
+++ src/app.js
@@ -1,4 +1,28 @@
-let apiKey = "e69t289845146b794bf2d43o9ea60040";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=metric`;
+// Search form
 
-console.log (apiUrl);
+function displayTemperature (position) {
+console.log(position.data.temperature.current)
+let temp = Math.round(position.data.temperature.current);
+let requestedTemperature = document.querySelector("#temperature");
+requestedTemperature.innerHTML = temp;
+let descriptionElement = document.querySelector("#description");
+descriptionElement.innerHTML=position.data.condition.description;
+}
+
+function searchCityData (city){
+let apiKey = "e69t289845146b794bf2d43o9ea60040";
+let units = "metric";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+axios.get(apiUrl).then(displayTemperature);
+}
+
+function submitSearchButton (event){
+event.preventDefault();
+let city = document.querySelector("h1");
+let cityInput = document.querySelector ("#search-city-input");
+city.innerHTML = cityInput.value;
+searchCityData(cityInput.value);
+}
+
+let form =document.querySelector("#search-form");
+form.addEventListener ("click", submitSearchButton);
