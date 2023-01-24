2a343b224082b69d1a565300121b1cf4a0f334dd
Odile-a <odilealvarez@gmail.com>
2023-01-24 21:15:18 +0000

    Showing current humidity and wind datas

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
 // Search form
 
-function displayTemperature (position) {
+
+
+function displayDatas (position) {
 console.log(position.data.temperature.current)
 let temp = Math.round(position.data.temperature.current);
-let requestedTemperature = document.querySelector("#temperature");
-requestedTemperature.innerHTML = temp;
+let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHTML = temp;
 let descriptionElement = document.querySelector("#description");
 descriptionElement.innerHTML=position.data.condition.description;
+let humidityElement = document.querySelector("#humidity");
+humidityElement.innerHTML=`${position.data.temperature.humidity}%`;
+let wind = Math.round(position.data.wind.speed);
+let windElement = document.querySelector("#wind");
+windElement.innerHTML=`${wind}km/h`;
 }
 
 function searchCityData (city){
 let apiKey = "e69t289845146b794bf2d43o9ea60040";
 let units = "metric";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
-axios.get(apiUrl).then(displayTemperature);
+axios.get(apiUrl).then(displayDatas);
 }
 
 function submitSearchButton (event){
