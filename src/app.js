858de43dc13257fb3de24367470b4268916a8064
Odile-a <odilealvarez@gmail.com>
2023-01-24 21:36:16 +0000

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
 // Search form
 
+function formatDate (timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+    hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+    minutes = `0${minutes}`;
+    }
+    let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
 
+}
 
 function displayDatas (position) {
-console.log(position.data.temperature.current)
+console.log(position.data.temperature.current);
 let temp = Math.round(position.data.temperature.current);
 let temperatureElement = document.querySelector("#temperature");
 temperatureElement.innerHTML = temp;
@@ -14,6 +36,9 @@ humidityElement.innerHTML=`${position.data.temperature.humidity}%`;
 let wind = Math.round(position.data.wind.speed);
 let windElement = document.querySelector("#wind");
 windElement.innerHTML=`${wind}km/h`;
+let dayElement=document.querySelector("#date");
+dayElement.innerHTML = (formatDate(position.data.time*1000));
+
 }
 
 function searchCityData (city){
