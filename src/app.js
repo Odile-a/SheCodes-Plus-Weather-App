175ec9e2e7694ddf5da599c4edbf997435418d3d
Odile-a <odilealvarez@gmail.com>
2023-01-24 22:29:49 +0000

    Added relevant icon from IPA

--- src/app.js
+++ src/app.js
@@ -34,12 +34,15 @@ humidityElement.innerHTML=`${position.data.temperature.humidity}%`;
 let wind = Math.round(position.data.wind.speed);
 let windElement = document.querySelector("#wind");
 windElement.innerHTML=`${wind}km/h`;
+let iconElement = document.querySelector("#icon");
+iconElement.setAttribute ( "src", `${position.data.condition.icon_url}`);
 let dayElement=document.querySelector("#date");
 dayElement.innerHTML = (formatDate(position.data.time*1000));
 
+
 }
 
-function searchCityData (city){
+function searchCityDatas (city){
 let apiKey = "e69t289845146b794bf2d43o9ea60040";
 let units = "metric";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
@@ -51,7 +54,7 @@ event.preventDefault();
 let city = document.querySelector("h1");
 let cityInput = document.querySelector ("#search-city-input");
 city.innerHTML = cityInput.value;
-searchCityData(cityInput.value);
+searchCityDatas(cityInput.value);
 }
 
 let form =document.querySelector("#search-form");
