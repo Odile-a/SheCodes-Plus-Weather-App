9a1f0bdbe827078802f3fe76a1123f5eae3f8eac
Odile-a <odilealvarez@gmail.com>
2023-01-26 13:51:49 +0000

    implemented the unit conversion

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+// Display current days and time
+
 function formatDate (timestamp) {
     let date = new Date(timestamp);
     let hours = date.getHours();
@@ -21,10 +23,18 @@ function formatDate (timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+
+// Display city datas after searching
+
 function displayDatas (position) {
-let temp = Math.round(position.data.temperature.current);
+let city = document.querySelector("h1");
+city.innerHTML = position.data.city;
+let tempCelsius = Math.round(position.data.temperature.current);
 let temperatureElement = document.querySelector("#temperature");
-temperatureElement.innerHTML = temp;
+celciusTemperature = Math.round(position.data.temperature.current);
+toFahrenheit.classList.remove("active");
+toCelsius.classList.add("active");
+temperatureElement.innerHTML = `${tempCelsius}`;
 let descriptionElement = document.querySelector("#description");
 descriptionElement.innerHTML=position.data.condition.description;
 let humidityElement = document.querySelector("#humidity");
@@ -36,11 +46,10 @@ let iconElement = document.querySelector("#icon");
 iconElement.setAttribute ( "src", `${position.data.condition.icon_url}`);
 let dayElement=document.querySelector("#date");
 dayElement.innerHTML = (formatDate(position.data.time*1000));
-
-
 }
 
-function searchCityDatas (city){
+//Calls the API url to get weather data based on city name searched
+function search (city){
 let apiKey = "e69t289845146b794bf2d43o9ea60040";
 let units = "metric";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
@@ -49,11 +58,46 @@ axios.get(apiUrl).then(displayDatas);
 
 function submitSearchButton (event){
 event.preventDefault();
-let city = document.querySelector("h1");
-let cityInput = document.querySelector ("#search-city-input");
-city.innerHTML = cityInput.value;
-searchCityDatas(cityInput.value);
+let cityInputElement = document.querySelector ("#search-city-input");
+search(cityInputElement.value);
 }
 
 let form =document.querySelector("#search-form");
 form.addEventListener ("submit", submitSearchButton);
+
+
+
+// units
+
+
+//Converts temp to Fahrenheit when "F" is clicked
+function displayFahrenheit (event) {
+    event.preventDefault();
+    toCelsius.classList.remove("active");
+    toFahrenheit.classList.add("active");
+    let temperatureElement = document.querySelector("#temperature");
+    let fahrenheitTemperature = (celciusTemperature * 9)/5 + 32;
+    temperatureElement.innerHTML=Math.round(fahrenheitTemperature);
+  }
+
+//Converts temp to Celsius when "C" is clicked  
+  function displayCelsius(event) {
+    event.preventDefault();
+    toFahrenheit.classList.remove("active");
+    toCelsius.classList.add("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celciusTemperature);
+  }
+
+//Global variable for Celsius temperature value
+let celciusTemperature = null;
+
+//Converts temp to Fahrenheit when "F" is clicked
+let toFahrenheit = document.querySelector("#f-link");
+toFahrenheit.addEventListener("click", displayFahrenheit);
+
+//Converts temp to Celsius when "C" is clicked
+let toCelsius = document.querySelector("#c-link");
+toCelsius.addEventListener("click", displayCelsius);
+
+search("Avignon");
